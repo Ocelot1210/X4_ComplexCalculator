5f7ec321690fe4db42b9369b4124418a3402d0fe
Ocelot <16898663+Ocelot1210@users.noreply.github.com>
2020-07-16 11:11:17 +0900

    設定タブ用のViewを追加

---
+++ X4_ComplexCalculator/Main/WorkArea/UI/StationSettings/StationSettings.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace X4_ComplexCalculator.Main.WorkArea.UI.StationSettings
+{
+    /// <summary>
+    /// StationSettings.xaml の相互作用ロジック
+    /// </summary>
+    public partial class StationSettings : UserControl
+    {
+        public StationSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
