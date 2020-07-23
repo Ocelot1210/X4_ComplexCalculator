17ff858e2e638c4010e208b9f34b45628b1fd6af
Ocelot <16898663+Ocelot1210@users.noreply.github.com>
2020-07-23 14:32:07 +0900

    Avalondockの代替ライブラリの使用により不要になった処理の削除

--- X4_ComplexCalculator/Main/WorkArea/WorkArea.xaml.cs
+++ X4_ComplexCalculator/Main/WorkArea/WorkArea.xaml.cs
@@ -14,13 +14,6 @@ namespace X4_ComplexCalculator.Main.WorkArea
         public WorkArea()
         {
             InitializeComponent();
-
-            // タブ切り替え/ドッキング/ドッキング解除するたびにコンストラクタが走るためDockingManagerをここで再設定する
-            // → 再設定しないと上記操作時にレイアウトが初期化される
-            if (Resources["ProxyDockingManager"] is BindingProxy proxy)
-            {
-                proxy.Data = dockingManager;
-            }
         }
     }
 }
