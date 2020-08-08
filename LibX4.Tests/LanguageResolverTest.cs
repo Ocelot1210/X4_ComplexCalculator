c4e5f2a7594727b88888ecba63e45d1d639e0272
Proudust <proudust@gmail.com>
2020-08-09 00:16:01 +0900

    Add more LanguageResolver test case

--- LibX4.Tests/LanguageResolverTest.cs
+++ LibX4.Tests/LanguageResolverTest.cs
@@ -53,7 +53,7 @@ namespace LibX4.Tests
 
 
         /// <summary>
-        /// 解決後の文字列に言語フィールド文字列が含まれる場合、再帰的に処理する
+        /// 解決後の文字列に言語フィールド文字列が含まれる場合、再帰的に解決する
         /// </summary>
         [Fact]
         public void Recursion()
@@ -69,6 +69,48 @@ namespace LibX4.Tests
             </language>
             ".ToXDocument()).Resolve("{1001,5802}");
             Assert.Equal("Planned Amount of Graphene", resolve);
+        }
+
+
+        /// <summary>
+        /// 解決後の文字列に言語フィールド文字列が含まれる場合、含まれなくなるまで再帰的に解決する
+        /// </summary>
+        [Fact]
+        public void DoubleSolution()
+        {
+            var resolve = new LanguageResolver(@"
+            <language>
+                <page id=""1001"">
+                     <t id=""5802"">Planned Amount of {20201, 1501}</t>
+                </page>
+                <page id=""20201"">
+                    <t id=""1501"">{20201, 1502}</t>
+                    <t id=""1502"">Graphene</t>
+                </page>
+            </language>
+            ".ToXDocument()).Resolve("{1001,5802}");
+            Assert.Equal("Planned Amount of Graphene", resolve);
+        }
+
+
+        /// <summary>
+        /// 解決後の文字列に言語フィールド文字列が 2 つ以上含まれる場合、全て解決する
+        /// </summary>
+        [Fact]
+        public void TwoPointResolve()
+        {
+            var resolve = new LanguageResolver(@"
+            <language>
+                <page id=""20101"">
+                    <t id=""10101"">Discoverer</t>
+                    <t id=""10102"">(Discoverer Vanguard){20101,10101} {20111,1101}</t>
+                </page>
+                <page id=""20111"">
+                    <t id=""1101"">Vanguard</t>
+                </page>
+            </language>
+            ".ToXDocument()).Resolve("{20101,10102}");
+            Assert.Equal("Discoverer Vanguard", resolve);
         }
 
 
@@ -104,6 +146,74 @@ namespace LibX4.Tests
             </language>
             ".ToXDocument()).Resolve("{1001,2490}");
             Assert.Equal("Shield Generators (including groups)", resolve);
+        }
+
+
+        /// <summary>
+        /// コメントと括弧が重なっている場合の解決 1
+        /// </summary>
+        [Fact]
+        public void MixingCommentsAndBrankets_1()
+        {
+            var resolve = new LanguageResolver(@"
+            <language>
+                <page id=""1"">
+                    <t id=""1"">aaaa\(bbbb(cccc)\)</t>
+                </page>
+            </language>
+            ".ToXDocument()).Resolve("{1,1}");
+            Assert.Equal("aaaa(bbbb)", resolve);
+        }
+
+
+        /// <summary>
+        /// コメントと括弧が重なっている場合の解決 2
+        /// </summary>
+        [Fact]
+        public void MixingCommentsAndBrankets_2()
+        {
+            var resolve = new LanguageResolver(@"
+            <language>
+                <page id=""1"">
+                    <t id=""1"">aaaa\(bbbb(cc\)cc)</t>
+                </page>
+            </language>
+            ".ToXDocument()).Resolve("{1,1}");
+            Assert.Equal("aaaa(bbbb", resolve);
+        }
+
+
+        /// <summary>
+        /// コメントと括弧が重なっている場合の解決 3
+        /// </summary>
+        [Fact]
+        public void MixingCommentsAndBrankets_3()
+        {
+            var resolve = new LanguageResolver(@"
+            <language>
+                <page id=""1"">
+                    <t id=""1"">aaaa(\(bbbb)cc\)cc</t>
+                </page>
+            </language>
+            ".ToXDocument()).Resolve("{1,1}");
+            Assert.Equal("aaaacc)cc", resolve);
+        }
+
+
+        /// <summary>
+        /// コメントと括弧が重なっている場合の解決 4
+        /// </summary>
+        [Fact]
+        public void MixingCommentsAndBrankets_4()
+        {
+            var resolve = new LanguageResolver(@"
+            <language>
+                <page id=""1"">
+                    <t id=""1"">aaaa(\(bbbb\)cc)cc</t>
+                </page>
+            </language>
+            ".ToXDocument()).Resolve("{1,1}");
+            Assert.Equal("aaaacc", resolve);
         }
     }
 }
