ea694ff0568154a486473f2effaabf44bcfd2958
Ocelot1210 <16898663+Ocelot1210@users.noreply.github.com>
2020-08-14 15:55:25 +0900

    fix #26 (#27)
    * 計画をインポートした際にレコードの編集状態が未編集になるように修正
    
    * ModInfoのxml宣言を読み飛ばす処理を修正
    
    * Modに含まれるcontent.xmlにxml宣言が記載されていない場合の考慮をした

--- LibX4/FileSystem/ModInfo.cs
+++ LibX4/FileSystem/ModInfo.cs
@@ -57,11 +57,12 @@ namespace LibX4.FileSystem
         /// <param name="modDirPath">Modのフォルダパス(絶対パスで指定すること)</param>
         public ModInfo(string modDirPath)
         {
-            // xml version="1.1"のModがまれに存在するため、XML宣言を読み飛ばす
-            using var sr = new StreamReader(Path.Combine(modDirPath, "content.xml"));
-            sr.ReadLine();
+            using var fs = new FileStream(Path.Combine(modDirPath, "content.xml"), FileMode.Open, FileAccess.Read);
 
-            var xml = XDocument.Load(sr);
+            // xml宣言を読み飛ばす
+            SkipXmlDefined(fs);
+
+            var xml = XDocument.Load(fs);
 
             ID      = xml.Root.Attribute("id")?.Value      ?? "";
             Name    = xml.Root.Attribute("name")?.Value    ?? "";
@@ -70,6 +71,89 @@ namespace LibX4.FileSystem
             Date    = xml.Root.Attribute("date")?.Value    ?? "";
             Enabled = xml.Root.Attribute("enabled")?.Value ?? "";
             Save    = xml.Root.Attribute("save")?.Value    ?? "";
+        }
+
+
+        /// <summary>
+        /// xml宣言を読み飛ばす
+        /// </summary>
+        /// <param name="fs">読み飛ばす対象</param>
+        /// <remarks>
+        /// xml version="1.1"のModがまれに存在するため、XML宣言を読み飛ばす
+        /// </remarks>
+        private static void SkipXmlDefined(FileStream fs)
+        {
+            var sr = new StreamReader(fs);
+
+            // xml宣言が記載されているか判定し、記載されていなければ何もしないで抜ける
+            {
+                char[] buff = new char[5];
+
+                sr.Read(buff, 0, buff.Length);
+
+                if (new string(buff) != "<?xml")
+                {
+                    fs.Position = 0;
+                    return;
+                }
+                fs.Position = buff.Length;
+            }
+
+            var isDblQt = false;        // ダブルクォート内か
+            var isSngQt = false;        // シングルクォート内か
+            var endOfXmlDef = false;    // xml宣言を読み飛ばし終えたか
+            var seek = 0;               // シーク量
+
+            // xml宣言を読み飛ばす
+            do
+            {
+                var chr = sr.Read();
+                seek++;
+                if (chr < 0)
+                {
+                    break;
+                }
+
+                switch ((char)chr)
+                {
+                    case '\'':
+                        // ダブルクォートで囲われていないシングルクォートか？
+                        if (!isDblQt)
+                        {
+                            isSngQt = !isSngQt;
+                        }
+                        break;
+
+                    case '\"':
+                        // シングルクォートで囲われていないダブルクォートか？
+                        if (!isSngQt)
+                        {
+                            isDblQt = !isDblQt;
+                        }
+                        break;
+
+                    case '?':
+                        // ダブルクォートまたはシングルクォートに囲われていない'?'か？
+                        if (!isSngQt && !isDblQt)
+                        {
+                            chr = sr.Read();
+                            seek++;
+
+                            // xml宣言の終了か？
+                            if (chr < 0 || (char)chr == '>')
+                            {
+                                endOfXmlDef = true;
+                            }
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            while (!sr.EndOfStream && !endOfXmlDef);
+
+            fs.Position += seek;
         }
     }
 }
