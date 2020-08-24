b4c18e5f1d979d33f29ce3927cb72b2a0824cbda
Proudust <proudust@gmail.com>
2020-08-24 22:23:51 +0900

    Fix parsing failed in some CurrentCulture (#34)
    * Add #5 reproduction test
    
    * Fix NullReferenceException
    
    * Fix parsing failed in some CurrentCulture

---
+++ LibX4.Tests/XAttributeExtensionTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Xml.Linq;
+using LibX4.Xml;
+using Xunit;
+
+namespace LibX4.Tests
+{
+    /// <summary>
+    /// <see cref="XAttributeExtension"/> のテストクラス
+    /// </summary>
+    public class XAttributeExtensionTest
+    {
+        /// <summary>
+        /// 言語設定によっては double のパースに失敗する場合がある
+        /// 参照: <a href="https://github.com/Ocelot1210/X4_ComplexCalculator/issues/5">#5</a>
+        /// </summary>
+        [Fact]
+        public void ParseFailedInSomeCurrentCulture()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("fr-FR");
+
+            var time = new XAttribute("time", "1.5");
+            Assert.Equal(1.5, time.GetDouble());
+        }
+    }
+}
