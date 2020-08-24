6628882aae44f912f847ac912e54507d576420d8
Proudust <proudust@gmail.com>
2020-08-24 23:27:06 +0900

    Fix method name

--- LibX4.Tests/XDocumentExTest.cs
+++ LibX4.Tests/XDocumentExTest.cs
@@ -28,7 +28,7 @@ namespace LibX4.Tests
         /// </summary>
         /// <param name="source">文字列</param>
         /// <returns>UTF8 ストリーム</returns>
-        private Stream utf8(string source) => new MemoryStream(UTF8.GetBytes(source));
+        private Stream Utf8(string source) => new MemoryStream(UTF8.GetBytes(source));
 
 
         /// <summary>
@@ -36,7 +36,7 @@ namespace LibX4.Tests
         /// </summary>
         /// <param name="source">文字列</param>
         /// <returns>UTF8 ストリーム</returns>
-        private Stream utf8bom(string source)
+        private Stream Utf8Bom(string source)
             => new MemoryStream(BOM.Concat(UTF8.GetBytes(source)).ToArray());
 
 
@@ -46,20 +46,20 @@ namespace LibX4.Tests
         [Fact]
         public void Xml11CanBeRead()
         {
-            using var s0 = utf8("<root></root>");
+            using var s0 = Utf8("<root></root>");
             XDocumentEx.Load(s0);
 
-            using var s1 = utf8(@"<?xml version=""1.1""?><root></root>");
+            using var s1 = Utf8(@"<?xml version=""1.1""?><root></root>");
             XDocumentEx.Load(s1);
 
-            using var s2 = utf8(@"<?xml version=""1.1"" encoding=""UTF-8""?><root></root>");
+            using var s2 = Utf8(@"<?xml version=""1.1"" encoding=""UTF-8""?><root></root>");
             XDocumentEx.Load(s2);
 
-            using var s3 = utf8(
+            using var s3 = Utf8(
                 @"<?xml version=""1.1"" encoding=""UTF-8"" standalone=""no""?><root></root>");
             XDocumentEx.Load(s3);
 
-            using var s4 = utf8(
+            using var s4 = Utf8(
                 @"<?xml version=""1.1"" encoding=""UTF-8"" standalone=""yes""?><root></root>");
             XDocumentEx.Load(s4);
         }
@@ -71,20 +71,20 @@ namespace LibX4.Tests
         [Fact]
         public void Utf8BomCanBeRead()
         {
-            using var s0 = utf8bom("<root></root>");
+            using var s0 = Utf8Bom("<root></root>");
             XDocumentEx.Load(s0);
 
-            using var s1 = utf8bom(@"<?xml version=""1.1""?><root></root>");
+            using var s1 = Utf8Bom(@"<?xml version=""1.1""?><root></root>");
             XDocumentEx.Load(s1);
 
-            using var s2 = utf8bom(@"<?xml version=""1.1"" encoding=""UTF-8""?><root></root>");
+            using var s2 = Utf8Bom(@"<?xml version=""1.1"" encoding=""UTF-8""?><root></root>");
             XDocumentEx.Load(s2);
 
-            using var s3 = utf8bom(
+            using var s3 = Utf8Bom(
                 @"<?xml version=""1.1"" encoding=""UTF-8"" standalone=""no""?><root></root>");
             XDocumentEx.Load(s3);
 
-            using var s4 = utf8bom(
+            using var s4 = Utf8Bom(
                 @"<?xml version=""1.1"" encoding=""UTF-8"" standalone=""yes""?><root></root>");
             XDocumentEx.Load(s4);
         }
