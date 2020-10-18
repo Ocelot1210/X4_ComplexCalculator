fd1229ed61980c92e45e3f08b5060a4661b0f2d1
Ocelot <16898663+Ocelot1210@users.noreply.github.com>
2020-10-18 19:28:58 +0900

    一部のテキストボックスにテキストクリア用のボタンを追加

---
+++ X4_ComplexCalculator_CustomControlLibrary/ClearableWaterMarkTextBox/ClearableWaterMarkTextBox.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Xceed.Wpf.Toolkit;
+
+namespace X4_ComplexCalculator_CustomControlLibrary
+{
+    /// <summary>
+    /// ×ボタンで内容を消せるWaterMarkTextBox
+    /// </summary>
+    public class ClearableWaterMarkTextBox : WatermarkTextBox
+    {
+        #region メンバ
+        /// <summary>
+        /// 内容クリアボタン
+        /// </summary>
+        private Button? _ClearButton;
+        #endregion
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        static ClearableWaterMarkTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(ClearableWaterMarkTextBox),
+                new FrameworkPropertyMetadata(typeof(ClearableWaterMarkTextBox))
+            );
+        }
+
+
+        /// <inheritdoc/>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            // 前のテンプレートのコントロールの後処理
+            if (_ClearButton != null)
+            {
+                _ClearButton.Click -= ClearButton_Click;
+            }
+
+            // テンプレートからコントロールの取得
+            _ClearButton = GetTemplateChild("PART_ClearButton") as Button;
+
+            // イベントハンドラの登録
+            if (_ClearButton != null)
+            {
+                _ClearButton.Click += ClearButton_Click;
+            }
+        }
+
+
+        /// <summary>
+        /// テキストクリアボタンクリック時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Text = "";
+            Focus();        // WaterMarkが一瞬表示される対策用
+        }
+    }
+}
