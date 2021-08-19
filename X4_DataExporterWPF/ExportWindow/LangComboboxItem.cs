685e7227f852b12ca6d5b9744e96578b966e11be
Proudust <proudust@gmail.com>
2021-08-19 19:43:45 +0900

    Change LangComboboxItem to sealed record

--- X4_DataExporterWPF/ExportWindow/LangComboboxItem.cs
+++ X4_DataExporterWPF/ExportWindow/LangComboboxItem.cs
@@ -3,31 +3,7 @@
     /// <summary>
     /// 言語コンボボックス用アイテム
     /// </summary>
-    class LangComboboxItem
-    {
-        #region プロパティ
-        /// <summary>
-        /// 言語ID
-        /// </summary>
-        public int ID { get; }
-
-
-        /// <summary>
-        /// 言語名
-        /// </summary>
-        public string Name { get; }
-        #endregion
-
-
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
-        /// <param name="id">言語ID</param>
-        /// <param name="name">言語名</param>
-        public LangComboboxItem(int id, string name)
-        {
-            ID = id;
-            Name = name;
-        }
-    }
+    /// <param name="ID">言語ID</param>
+    /// <param name="Name">言語名</param>
+    sealed record LangComboboxItem(int ID, string Name);
 }
