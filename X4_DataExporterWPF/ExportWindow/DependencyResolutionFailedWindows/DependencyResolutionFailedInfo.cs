ccceabb587fe5f050f4ec7a6f845679655278da6
Ocelot <16898663+Ocelot1210@users.noreply.github.com>
2023-08-05 22:56:22 +0900

    依存関係の解決に失敗した Mod をより詳細に表示するようにした

---
+++ X4_DataExporterWPF/ExportWindow/DependencyResolutionFailedWindows/DependencyResolutionFailedInfo.cs
@@ -0,0 +1,57 @@
+﻿using Prism.Mvvm;
+
+namespace X4_DataExporterWPF.ExportWindow.DependencyResolutionFailedWindows
+{
+    /// <summary>
+    /// 依存関係の解決に失敗した情報を表すクラス
+    /// </summary>
+    internal class DependencyResolutionFailedInfo : BindableBase
+    {
+        /// <summary>
+        /// Mod名
+        /// </summary>
+        public string ModName { get; }
+
+
+        /// <summary>
+        /// インデント付き Mod 名
+        /// </summary>
+        public string ModNameWithIndent { get; }
+
+
+        /// <summary>
+        /// Mod ID
+        /// </summary>
+        public string ID { get; }
+
+
+        /// <summary>
+        /// Mod の格納先
+        /// </summary>
+        public string FolderName { get; }
+
+
+        /// <summary>
+        /// 備考
+        /// </summary>
+        public string Remarks { get; }
+
+
+        /// <summary>
+        /// 新しいインスタンスを作成する
+        /// </summary>
+        /// <param name="modName">Mod名</param>
+        /// <param name="id">Mod ID</param>
+        /// <param name="folderName">Mod の格納先</param>
+        /// <param name="remarks">備考</param>
+        /// <param name="level">依存関係の深さ</param>
+        internal DependencyResolutionFailedInfo(string modName, string id, string folderName, string remarks, int level)
+        {
+            ModNameWithIndent = "".PadRight(level * 4) + modName;
+            ModName = modName;
+            ID = id;
+            FolderName = folderName;
+            Remarks = remarks;
+        }
+    }
+}
---
+++ X4_DataExporterWPF/ExportWindow/DependencyResolutionFailedWindows/DependencyResolutionFailedWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LibX4.FileSystem;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace X4_DataExporterWPF.ExportWindow.DependencyResolutionFailedWindows
+{
+    /// <summary>
+    /// DependencyResolutionFailedWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DependencyResolutionFailedWindow : Window
+    {
+        public DependencyResolutionFailedWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// 新しいインスタンスを作成する
+        /// </summary>
+        /// <param name="mods">依存関係の解決に失敗した Mod の一覧</param>
+        public DependencyResolutionFailedWindow(IReadOnlyList<ModInfo> mods)
+        {
+            InitializeComponent();
+
+            DataContext = new DependencyResolutionFailedModel(mods);
+        }
+    }
+}
